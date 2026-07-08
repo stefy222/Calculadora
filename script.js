9acfdb873f885e317eb1c85c1c1c6587a92411da
Rosibel Paucara <stefpau220@gmail.com>
2026-07-08 17:28:00 -0400

    localStorage

--- script.js
+++ script.js
@@ -8,38 +8,58 @@ let primerValor = "";
 let segundoValor = "";
 let operador = "";
 
-function limpiarDisplay(){
+// Recuperar historial del localStorage
+let operaciones = localStorage.getItem("operaciones");
+
+if (operaciones == null) {
+    operaciones = "";
+} else {
+    let historial = operaciones.split("\n");
+
+    historial.forEach(function(operacion) {
+        if (operacion != "") {
+            const li = document.createElement("li");
+            li.textContent = operacion;
+            listaHistorial.appendChild(li);
+        }
+    });
+}
+
+function limpiarDisplay() {
     display.value = "";
 }
 
-function guardarOperacion(valor){
+function guardarOperacion(valor) {
     primerValor = display.value;
     operador = valor;
     display.value = "";
 }
 
-function agregarNumero(valor){
+function agregarNumero(valor) {
     display.value += valor;
 }
 
-function esOperador(valor){
+function esOperador(valor) {
     return valor === "+" ||
            valor === "-" ||
            valor === "*" ||
            valor === "/";
 }
 
-buttons.forEach(function(button){
-    button.addEventListener("click", function(){
+buttons.forEach(function(button) {
+
+    button.addEventListener("click", function() {
+
         let value = button.dataset.value;
-        
-        if(esOperador(value)){
+
+        if (esOperador(value)) {
             guardarOperacion(value);
-        }
-        else{
+        } else {
             agregarNumero(value);
         }
+
     });
+
 });
 
 clearButton.addEventListener("click", limpiarDisplay);
@@ -63,8 +83,17 @@ function calcularResultado() {
             resultado = parseFloat(primerValor) / parseFloat(segundoValor);
             break;
     }
+
     display.value = resultado;
+
+    let operacion = `${primerValor} ${operador} ${segundoValor} = ${resultado}`;
+
     const elementoHistorial = document.createElement("li");
-    elementoHistorial.textContent = `${primerValor} ${operador} ${segundoValor} = ${resultado}`;
+    elementoHistorial.textContent = operacion;
     listaHistorial.appendChild(elementoHistorial);
+
+    // Guardar en localStorage
+    operaciones += operacion + "\n";
+    localStorage.setItem("operaciones", operaciones);
+
 }
