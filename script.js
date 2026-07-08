6172401443ee540437c392932ea7d6f49cb379c7
Rosibel Paucara <stefpau220@gmail.com>
2026-07-07 20:56:07 -0400

    Third commit: calculo de valores

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
-const display = document.getElementById('display');
-const buttons = document.querySelectorAll('button');
-const clearButton = document.getElementById('clear');
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+const clearButton = document.getElementById("clear");
+const equalsButton = document.getElementById("equals");
 
-buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
+let primerValor = "";
+let segundoValor = "";
+let operador = "";
+
+function limpiarDisplay(){
+    display.value = "";
+}
+
+function guardarOperacion(valor){
+    primerValor = display.value;
+    operador = valor;
+    display.value = "";
+}
+
+function agregarNumero(valor){
+    display.value += valor;
+}
+
+function esOperador(valor){
+    return valor === "+" ||
+           valor === "-" ||
+           valor === "*" ||
+           valor === "/";
+}
+
+buttons.forEach(function(button){
+    button.addEventListener("click", function(){
         let value = button.dataset.value;
-        display.value += value;
-    })
+        if(esOperador(value)){
+            guardarOperacion(value);
+        }
+        else{
+            agregarNumero(value);
+        }
+    });
 });
 
-clearButton.addEventListener('click', function() {
-    display.value = '';
-});
+clearButton.addEventListener("click", limpiarDisplay);
+
+equalsButton.addEventListener("click", calcularResultado);
+
+function calcularResultado(){
+    segundoValor = display.value;
+    if(operador === "+"){
+        display.value = parseFloat(primerValor) + parseFloat(segundoValor);
+    }
+    else if(operador === "-"){
+        display.value = parseFloat(primerValor) - parseFloat(segundoValor);
+    }
+    else if(operador === "*"){
+        display.value = parseFloat(primerValor) * parseFloat(segundoValor);
+    }
+    else if(operador === "/"){
+        display.value = parseFloat(primerValor) / parseFloat(segundoValor);
+    }
+}
